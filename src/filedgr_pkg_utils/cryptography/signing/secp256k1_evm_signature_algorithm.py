e67142b021fdf4d03dd2ab0484d6a4e38bd37bbe
Esquive <e.falk@niugroup.lu>
2026-01-04 02:27:19 +0100

    added evm style signing

---
+++ src/filedgr_pkg_utils/cryptography/signing/secp256k1_evm_signature_algorithm.py
@@ -0,0 +1,23 @@
+from ecdsa import SigningKey, SECP256k1, VerifyingKey, BadSignatureError
+from eth_account import Account
+
+
+class Secp256k1EvmAlgorithm:
+
+    def __init__(self,
+                 sign_key_hex: str = None,
+                 verify_key_hex: str = None):
+        if sign_key_hex:
+            self.__priv = sign_key_hex if sign_key_hex.startswith("0x") else "0x" + sign_key_hex
+        if verify_key_hex:
+            self.__pub = VerifyingKey.from_string(bytes.fromhex(verify_key_hex), curve=SECP256k1)
+
+    def sign(self, digest: str) -> str:
+        digest = bytes.fromhex(digest[2:] if digest.startswith("0x") else digest)
+        if len(digest) != 32:
+            raise ValueError("digest must be 32 bytes")
+        signed = Account.unsafe_sign_hash(digest, private_key=self.__priv)
+        return signed.signature.hex()
+
+    def verify(self, digest: bytes, signature: bytes) -> bool:
+        raise NotImplementedError()
